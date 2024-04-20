f29e7ebcc010b9a3ef7349b84ef0721364a5f773
imaqeelkhan28 <imaqeelkhan28@gmail.com>
2024-04-21 02:53:54 +0500

    login and logout user with toastr

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='Home'),
-    path('about', views.aboutUs, name='About'),
-    path('contact', views.contact, name='Contact'),
+    path('about/', views.aboutUs, name='About'),
+    path('contact/', views.contact, name='Contact'),
+    path('login', views.login, name='Login'),
+    path('logout/', views.logoutUser, name='Logout'),
 ]
