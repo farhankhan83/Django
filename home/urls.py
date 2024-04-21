bae8f2016550fd8f2273c73db30487262229a994
femioyeranmi03 <>
2024-04-22 02:03:55 +0500

    course page and details page added

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.homePage, name='Home'),
     path('about/', views.aboutUs, name='About'),
+    path('courses/', views.courses, name='Courses'),
+    path('courses/<int:course_id>', views.courseDetail, name='CourseDetail'),
     path('contact/', views.contact, name='Contact'),
     path('login', views.login, name='Login'),
     path('logout/', views.logoutUser, name='Logout'),
