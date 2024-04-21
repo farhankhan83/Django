00e7b1a93a03b2181f60e540b832e9775c4634a3
farhankhan83 <160052001+farhankhan83@users.noreply.github.com>
2024-04-21 19:59:02 +0100

    Aqeel/login and logout (#3)
    * added db config and implemented env variables
    
    * login and logout user with toastr
    
    ---------
    
    Co-authored-by: imaqeelkhan28 <imaqeelkhan28@gmail.com>

--- home/views.py
+++ home/views.py
@@ -1,15 +1,56 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.template import loader
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib import messages
 
 def homePage(request):
-    template = loader.get_template('homePage.html')
-    return HttpResponse(template.render())
+    return render(request, 'homePage.html', {})
 
 def aboutUs(request):
-    template = loader.get_template('about.html')
-    return HttpResponse(template.render())
+    return render(request, 'about.html', {})
 
 def contact(request):
-    template = loader.get_template('contact.html')
-    return HttpResponse(template.render())
+    return render(request, 'contact.html', {})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authentication Logic
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            auth_login(request, user)
+            messages.success(request, 'User Logged In Successfully')
+            return redirect('Home')
+        else:
+            messages.success(request, "Error Logging In")
+            return redirect('Home')
+    else:
+        return render(request, 'login.html', {})
+
+
+def loginUser(request):
+    print(request)
+
+    if request.method == 'POST':
+        email = request.POST['username']
+        password = request.POST['password']
+
+        # Authentication Logic
+        user = authenticate(request, useremail = email, password = password)
+        if user is not None:
+            login(request, user)
+            message.success(request, 'User Logged In Successfully')
+            return redirect('Home')
+        else:
+            messages.success(request, "Error Logging In")
+            return redirect('Home')
+    else:
+        return render(request, 'home.html', {})
+
+def logoutUser(req):
+    logout(req)
+    messages.success(req, 'Logged out successfully!')
+    return redirect('Home')
