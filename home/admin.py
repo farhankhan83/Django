e4fb1777196e400157892fbddbf7bbaf8c4cdca2
farhankhan83 <160052001+farhankhan83@users.noreply.github.com>
2024-04-21 22:15:08 +0100

    all necessary models created (#5)

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Student, Module, Course, Category, Registration
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Module)
+admin.site.register(Category)
+admin.site.register(Registration)
+admin.site.register(Course)
